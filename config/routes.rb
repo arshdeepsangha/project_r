16095f5527f416c571524dcb604bad90b05d6ead
arsh98000 <arsh98000@gmail.com>
2019-12-05 23:04:22 -0600

    Setting up a root

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/:permalink' , to: 'pages#permalink' , as: 'permalink'
 
+  root 'pages#show', {id: 1}
+
 end
