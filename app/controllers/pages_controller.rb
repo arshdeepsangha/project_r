fac2b85834ef42560bf5ece57e36b8d72ff3829f
arsh98000 <arsh98000@gmail.com>
2019-12-05 22:31:22 -0600

    Added Static pages and sorry page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,16 @@
 class PagesController < ApplicationController
   def show
+    @page = Page.find(params[:id])
+  end
+
+  def permalink
+
+    @page = Page.find_by(permalink: params[:permalink])
+
+        if @page.nil?
+        render :sorry
+        else
+        render :show
+        end
   end
 end
